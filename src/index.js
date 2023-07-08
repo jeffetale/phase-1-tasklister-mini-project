799051aa57704fa4ac0fcab30fc20b23a3771ca1
jeff <jeff.etale@student.moringaschool.com>
2023-07-09 02:53:06 +0300

    users can enter data and list items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const tasksList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const taskInput = document.querySelector('#new-task-description');
+    const taskDescription = taskInput.value;
+
+    if (taskDescription) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+      tasksList.appendChild(taskItem);
+      taskInput.value = '';
+    }
+  });
+  
 });
+
+ 
