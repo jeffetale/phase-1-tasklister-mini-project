060eb2c9ba09b5973381b43d8627dde51799f82d
jeff <jeff.etale@student.moringaschool.com>
2023-07-09 03:06:56 +0300

    Added a delete button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#create-task-form');
   const tasksList = document.querySelector('#tasks');
 
   form.addEventListener('submit', (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const taskInput = document.querySelector('#new-task-description');
     const taskDescription = taskInput.value;
@@ -11,11 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
     if (taskDescription) {
       const taskItem = document.createElement('li');
       taskItem.textContent = taskDescription;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);
       tasksList.appendChild(taskItem);
       taskInput.value = '';
     }
   });
-  
 });
-
- 
